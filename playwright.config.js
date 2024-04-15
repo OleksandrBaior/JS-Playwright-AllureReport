7e8405c90e15aa7add6864d25e6d2a2239dbe005
OleksandrBaior <bayor.olexandr@gmail.com>
2024-04-15 13:16:30 +0200

    Add timeout global

--- playwright.config.js
+++ playwright.config.js
@@ -1,19 +1,16 @@
 // @ts-check
 const { defineConfig, devices } = require('@playwright/test');
 import * as os from "os";
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
+  /* Set timeout */
   timeout: 40000,
-  
+  expect: {
+    timeout: 10 * 1000,
+  },
+    /* Folder of tests */
   testDir: './tests/specs',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -41,22 +38,17 @@ module.exports = defineConfig({
       },
     ],
   ],
-
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://www.redmine.org/',
-
     actionTimeout: 15000, 
     navigationTimeout: 15000,
-    
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
     video: 'retain-on-failure',
     screenshot: 'only-on-failure'
   },
-
   /* Configure projects for major browsers */
   projects: [
     {
